3fce3c3cd9994892c2b39e4bb87aaa424ecbf7eb
Piyal <piyal.abrarmahmud@gmail.com>
2023-09-07 23:30:03 +0600

    First1

---
+++ 1905097_Offline_4.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string data_string;
+    cout << "enter data string: ";
+    getline(cin, data_string);
+    cout << data_string << endl;
+    return 0;
+}
